6bfe2049520a7e97f4a316497f16b8a946f1e6c2
shishido1852 <revelhotline@gmail.com>
2021-03-12 09:35:53 +0900

    課題　条件分岐・演算子
    問1.問2.問3

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,26 @@
+let a = 10;
+if (a !=  10 ) {
+    console.log('aは10ではありません。'); 
+}else {
+    console.log('aは10です。');  
+}
+let b = 18;
+if (b >= 10 && b < 20　&&　b % 2 === 0) {
+    console.log('bは10以上20未満、かつ偶数である。');  
+}else {
+    console.log('bは10以上20未満、かつ偶数ではありません。');  
+}
+
+let x = 10;
+if (x >= 10 && x <= 20) {
+    console.log('「成功です」');  
+}else {
+    console.log('「失敗です」');  
+}
+
+let y = 0;
+if (y % 2 === 0) {
+    console.log('「偶数です」');  
+}else {
+    console.log('「奇数です」');  
+}
