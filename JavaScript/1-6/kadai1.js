9455f2dc0fe7dd4ab34a10cfea2074c04eeedd9e
shishido1852 <revelhotline@gmail.com>
2021-03-22 09:25:52 +0900

    課題　配列・オブジェクト

--- JavaScript/1-6/kadai1.js
+++ JavaScript/1-6/kadai1.js
@@ -1,4 +1,5 @@
 let scores = [10, 15, 20, 25];
+console.log(scores);
 for (let i = 0; i < scores.length; i++) {
     if (scores[i] % 2 === 0) {
         console.log(scores[i] + "は偶数です");
--- JavaScript/1-6/kadai2.js
+++ JavaScript/1-6/kadai2.js
@@ -2,6 +2,7 @@ let car = {
     gass:20.5,
     num:1234,
 };
+console.log(car);
 let g = (car.gass)
 console.log("ガソリンは" + g + "です");
 let n = (car.num)
